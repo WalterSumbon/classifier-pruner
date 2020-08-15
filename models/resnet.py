254c6c86d9ed61f5cf9ae5e49055c9a0992cb3a1
waltersumbon <907449377@qq.com>
2020-08-15 20:23:22 +0800

    remove unused classes and add ResNetCFG_cifar

--- models/resnet.py
+++ models/resnet.py
@@ -11,34 +11,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from copy import deepcopy
-
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-
-        self.shortcut = nn.Sequential()
-        
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
     
 class PrunableBasicBlock(nn.Module):
     expansion = 1
@@ -69,38 +41,6 @@ class PrunableBasicBlock(nn.Module):
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion *
-                               planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
-
-        self.shortcut = nn.Sequential()
-        
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
         out += self.shortcut(x)
         out = F.relu(out)
         return out
@@ -141,39 +81,6 @@ class PrunableBottleneck(nn.Module):
         out = F.relu(out)
         return out
 
-
-class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(ResNet, self).__init__()
-        self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
-
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
 
 _num_classes = {
     'cifar10' : 10,
@@ -291,7 +198,7 @@ class PrunableResNet_imagenet(nn.Module):
         return self.module_list, self.shortcutCB_list
         
 
-class ResNetCFG_imagenet:
+class ResNetCFG_imagenet:   #包含各种结构信息的类
     def __init__(self):
         self.cfg18 = [[64], [64, 64], [64, 64], [128, 128], [128, 128], [256, 256], [256, 256], [512, 512], [512, 512]]
         self.cfg34 = [[64], [64, 64], [64, 64], [64, 64], [128, 128], [128, 128], [128, 128], [128, 128], [256, 256], \
@@ -455,10 +362,10 @@ def ResNet152_imagenet(cfg=None):
     else:
         return PrunableResNet_imagenet(PrunableBottleneck, [3, 8, 36, 3], cfg=cfg)
 
-class ResNet_CIFAR(nn.Module):
+class ResNet_cifar(nn.Module):
     def __init__(self, num_layers, num_classes, cfg=None):
         print('[num_classes: %d, num_layers: %d]', num_classes, num_layers)
-        super(ResNet_CIFAR, self).__init__()
+        super(ResNet_cifar, self).__init__()
         assert (num_layers-2)%6 == 0
         _n = (num_layers-2)//6
         if cfg is not None:
@@ -533,6 +440,48 @@ class ResNet_CIFAR(nn.Module):
                 
         return self.module_list, self.shortcutCB_list
 
+class ResNetCFG_cifar:   #包含各种结构信息的类
+    def __init__(self,dataset):
+        self.dataset = dataset
+
+    def get_cfg(self,raw_model):
+        layers = 1 # '1' is linear layer
+        for name,params in raw_model.named_parameters():
+            if 'conv' in name:
+                layers = layers + 1
+        
+        if layers in [18, 34]:
+            block_convs = 2
+        else:
+            block_convs = 3
+        
+        first_conv_flag = True
+        cfg = []
+        block = []
+        count = 0
+        for name,params in raw_model.named_parameters():
+            if 'conv' in name:
+                if first_conv_flag: # first conv's output channels
+                    block = [params.shape[0]] 
+                    cfg.append(deepcopy(block))
+                    block.clear()
+                    first_conv_flag = False
+                else: # all conv's output channels in a block, but shortcut's conv is not included  
+                    block.append(params.shape[0])
+                    count = count + 1
+                    if count == block_convs:
+                        cfg.append(deepcopy(block))
+                        block.clear()
+                        count = 0
+        return cfg
+        
+    def get_prune_idx(self, model): 
+        layers = 1 # '1' is linear layer
+        for name,params in model.named_parameters():
+            if 'conv' in name:
+                layers = layers + 1
+        assert (layers-2)%6 == 0
+
 def PrunableResNet(num_layers, dataset, cfg=None):
     '总的可剪枝残差网络生成入口'
     assert dataset in ['imagenet','cifar10','cifar100']
@@ -541,5 +490,4 @@ def PrunableResNet(num_layers, dataset, cfg=None):
         return globals()['ResNet%d_imagenet'%num_layers](cfg)
     else:
         num_classes = _num_classes[dataset]
-        return ResNet(num_layers, num_classes, cfg)
-    
+        return ResNet_cifar(num_layers, num_classes, cfg)
